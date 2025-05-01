9194128e7db3c614617e7493464e6266ab56a529
Otávio Carvalho <carvalho07otavioh@gmail.com>
2025-05-01 00:07:28 -0300

    Estrutura de Dados

---
+++ projeto.py
@@ -0,0 +1,12 @@
+voo = {
+    "código": "LAT1234",
+    "destino": "São Paulo",
+    "horário": "14:30"
+}
+
+companhia = {
+    "nome": "LATAM",
+    "voos": [voo1, voo2, voo3, ...]  # Pilha: topo = voo mais próximo
+}
+
+fila_companhias = [companhia1, companhia2, ...]
