0d2f17d716255249f629274d11f995c268e95aa8
Otávio Carvalho <carvalho07otavioh@gmail.com>
2025-05-01 14:43:01 -0300

    FunÇão Inserir Companhia

--- projeto.py
+++ projeto.py
@@ -6,7 +6,19 @@ voo = {
 
 companhia = {
     "nome": "LATAM",
-    "voos": [voo1, voo2, voo3, ...]  # Pilha: topo = voo mais próximo
+    "voos": [voo1, voo2, voo3, ...]
 }
 
 fila_companhias = [companhia1, companhia2, ...]
+
+def inserirCompanhia(fila, nomeCompanhia):
+    companhia = {
+        "nome": nomeCompanhia,
+        "voos": []
+    }
+    fila.append(companhia)
+    print(f'Companhia "{nomeCompanhia}" adicionada à fila.')
+
+fila_companhias = []
+inserirCompanhia(fila_companhias, "LATAM")
+inserirCompanhia(fila_companhias, "GOL")
